2eb00cf6b83425f4a583a351d407a3e660102d9d
RS <rspiegelman@nvidia.com>
2025-11-11 14:21:25 +0200

    workign Idans algo
    Signed-off-by: RS <rspiegelman@nvidia.com>

--- kvhash_simulation.py
+++ kvhash_simulation.py
@@ -1,66 +1,112 @@
 #!/usr/bin/env python3
 
+
+
+
 import argparse
 import random
+import math
 from collections import defaultdict
 
 
-def main(num_blocks_in_storage, kvcache_len_blocks, num_requests, iterations):    
-    reqs_to_blocks = defaultdict(lambda: [-1] * kvcache_len_blocks)
-    block_to_request = defaultdict(lambda: -1)
-    hit_counter = 0
-    for i in range(iterations):
-        curr_request = i % num_requests
-        for block in reqs_to_blocks[curr_request]:
-            if block_to_request[block] == curr_request:
-                hit_counter += 1 
-        reqs_to_blocks[curr_request] = random.choices(range(num_blocks_in_storage), k=kvcache_len_blocks)
-        for block in reqs_to_blocks[curr_request]:
-            block_to_request[block] = curr_request
+class Conversation:
+    def __init__(self):
+        self.uses = 0
+
+
+class Simulation:
+    def __init__(self, blocks_in_storage, inflight_conversations, conversation_length, total_conversations, ranges) -> None:
+        if blocks_in_storage % ranges != 0:
+            print(f"Error: blocks_in_storage ({blocks_in_storage}) must be divisible by ranges ({ranges})")
+            exit(1)
+        
+        self.blocks_in_storage = blocks_in_storage
+        self.inflight_conversations = inflight_conversations
+        self.conversation_length = conversation_length
+        self.total_conversations = total_conversations
+        self.ranges = ranges
+
+        self.inflights = dict() #convid->conversation
+        self.conversationid_to_blocks = defaultdict(lambda: set()) #convid->block
+        self.block_to_conversationid = defaultdict(lambda: -1) #block->convid
+        self.hit_miss = {'hit' : 0, 'miss' :0}
+
+        self.range_len = self.blocks_in_storage // self.ranges
+        self.num_writes = 0
+        self.consecutive_writes_in_range = math.ceil(self.inflight_conversations / self.ranges)
+        self.next_conv_id = 0
+
     
-    cache_hit_ratio = hit_counter / (iterations * kvcache_len_blocks)
-    print(f"Cache Hit Ratio: {cache_hit_ratio:.4f} ({hit_counter}/{iterations * kvcache_len_blocks})")
-     
+    def touch_conversation(self, conv_id):
+        conversation = self.inflights[conv_id]
+        if (conversation.uses > 0) and (len(self.inflights) == self.inflight_conversations):
+           self.hit_miss['hit' if len(self.conversationid_to_blocks[conv_id]) > 0 else 'miss'] += 1  
+        if len(self.conversationid_to_blocks[conv_id]) == 0: 
+            curr_range = (self.num_writes // self.consecutive_writes_in_range) % self.ranges
+            block = curr_range * self.range_len + random.randrange(self.range_len)
+            #print(f"Writing to range {curr_range} block {block}")
+            self.conversationid_to_blocks[self.block_to_conversationid[block]].discard(block)
+            self.conversationid_to_blocks[conv_id].add(block) 
+            self.block_to_conversationid[block] = conv_id
+            self.num_writes += 1
+        conversation.uses += 1
+    
+    def add_new_conversation(self):
+        self.inflights[self.next_conv_id] = Conversation() 
+        self.touch_conversation(self.next_conv_id)
+        self.next_conv_id += 1
+        return self.next_conv_id - 1
+
+    def run(self):
+        finished_conversations = 0
+        while finished_conversations < self.total_conversations:
+            if len(self.inflights) < self.inflight_conversations:
+                conv_id = self.add_new_conversation()
+            else:
+                conv_id = random.choice(list(self.inflights.keys())) 
+                self.touch_conversation(conv_id)
+            if self.inflights[conv_id].uses >= self.conversation_length:
+                finished_conversations += 1
+                del self.inflights[conv_id]
+        print (self.hit_miss)   
+            
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(
         description="Simulate KV cache behavior with configurable parameters"
     )
-    
     parser.add_argument(
-        "--num_blocks_in_storage",
+        "--blocks_in_storage",
         type=int,
         required=True,
         help="Number of blocks available in storage"
-    )
-    
+    )   
     parser.add_argument(
-        "--kvcache_len_blocks",
+        "--inflight_conversations",
         type=int,
         required=True,
-        help="Length of KV cache in blocks"
+        help="each conversation is a single block, this paramter says how many inflight conversations are"
     )
-    
     parser.add_argument(
-        "--num_requests",
+        "--conversation_length",
         type=int,
         required=True,
-        help="Number of inflight requests, this simulates the number of alive requests"
-    )
-    
+        help="how much folow up questions are in one conversation"
+    ) 
     parser.add_argument(
-        "--iterations",
+        "--total_conversations",
         type=int,
         required=True,
-        help="Number of iterations to simulate"
+        help="how many conversations in total to process"
+    ) 
+    parser.add_argument(
+        "--ranges",
+        type=int,
+        required=True,
+        help="num of ranges to split the block storage"
     )
     
     args = parser.parse_args()
-    main(
-        num_blocks_in_storage=args.num_blocks_in_storage,
-        kvcache_len_blocks=args.kvcache_len_blocks,
-        num_requests=args.num_requests,
-        iterations=args.iterations
-    )
+    s = Simulation(blocks_in_storage=args.blocks_in_storage, inflight_conversations=args.inflight_conversations, conversation_length=args.conversation_length, total_conversations=args.total_conversations, ranges=args.ranges)
+    s.run()
 
